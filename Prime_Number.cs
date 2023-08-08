fd918619b0cea54b36d9eb53361f6ef560427331
amrit-maurya <139717568+amrit-maurya@users.noreply.github.com>
2023-08-08 19:26:19 +0530

    Check_Prime_Number.cs

--- Prime_Number.cs
+++ Prime_Number.cs
@@ -36,3 +36,13 @@ namespace PrimeNumberChecker
         }
     }
 }
+/*
+When checking for the primality of a number num, we don't need to test divisibility by numbers greater than its square root.
+This is because if num is not a prime number and has a divisor larger than its square root,
+it must also have a corresponding divisor smaller than its square root.
+So, by checking divisors up to the square root of num,
+we cover all possible divisors and can determine whether num is prime or not efficiently.
+Using Math.Sqrt(num) in the loop condition helps us avoid unnecessary iterations and makes the primality check more efficient.
+We only need to iterate up to the square root of the number being checked,
+which reduces the number of divisions we need to perform and speeds up the process.
+*/
